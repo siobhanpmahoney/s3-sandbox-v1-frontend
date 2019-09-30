c7c664b6265d2081f262ce271dd0c1c9604ad2fb
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2019-09-29 21:01:46 -0400

    able to send files to backend

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      example: {},
       files: []
     }
+    this.fileInput = React.createRef();
+    this.onAddFileData = this._onAddFileData.bind(this)
   }
 
   addFileToState = (file) => {
-    debugger
+
     let stateFiles = [...this.state.files, file]
     this.setState({
       files: stateFiles
@@ -25,16 +28,124 @@ class App extends React.Component {
 
 
 
+  _onAddFileData = (event) => {
+
+    const file = this.fileInput.current.files[0]
+    const reader = new FileReader()
+
+    let fileUploadState = this.state.files.splice(0)
+    fileUploadState = [...this.fileInput.current.files]
+    console.log("fileUploadState", fileUploadState)
+
+    reader.onloadend = () => {
+      this.setState({
+        example: {url: reader.result},
+        files: fileUploadState
+      })
+      // this.setState({
+      //   files:
+      // })
+    }
+
+    if (file) {
+      reader.readAsDataURL(file)
+      let url = reader.result
+      this.setState({
+        example: {url: url},
+        files: fileUploadState
+      }, console.log("state?"), this.state)
+    }
+    let name = event.target.name
+    let value = event.target.value
+    let ex = Object.assign({}, this.state.example )
+    ex[name] = value
+    this.setState({
+      example: ex
+    }, console.log(ex))
+  }
+
+  sendToS3 = (event) => {
+    event.preventDefault()
+
+    // fetch('http://localhost:3000/api/v1/albums', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //     'Accepts': 'application/json'
+    //   },
+    //   body: JSON.stringify({title: "album4x"})
+    // })
+    // .then(response => response.json())
+    // .then(album => {
+    //   fetch('http://localhost:3000/api/v1/songs', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-type': 'application/json',
+    //       'Accepts': 'application/json'
+    //     },
+    //     body: JSON.stringify({title: `${album.title}/song4000`, album_id: album.id })
+    //   })
+    //   .then(res => res.json())
+    //   .then(song => {
+        // formdata.append('title', `album4x/song4000/${Date.now()}`)
+        // formdata.append('song_id', 4)
+
+    let formdata = new FormData();
+
+        formdata.append('song_id', '1')
+        formdata.append('file', this.state.files[0])
+        console.log(this.state.files[0])
+
+        fetch('http://localhost:3000/api/v1/versions', {
+          method: 'POST',
+          body: formdata
+        })
+        .then(rez => rez.json())
+        .then(j => console.log(j))
+    //   })
+    // })
+
+
+  }
+
+
+
+
   render() {
     console.log("state: ", this.state)
     return(
       <div>
-        <div>Dropzone hook:</div>
-        <MyDropzone addFileToState = {this.addFileToState}/>
+        <div>
+          <h3>Vanilla HTML form</h3>
+
+        <form onSubmit={this.sendToS3}>
+          <input onChange={this.onAddFileData} type="file" name="file" ref={this.fileInput} multiple/>
+            <button type="submit">Fake Submit</button>
+
+        </form>
+
+        </div>
+
+
+          <hr />
+        <div>
+          <h3>Dropzone 1:</h3>
+          <MyDropzone addFileToState = {this.onAddFileData}/>
+        </div>
 
         <hr />
 
-      <MyDropzone2 addFileToState = {this.addFileToState}/>
+      <div>
+        <h3>Dropzone 1:</h3>
+        <MyDropzone2 addFileToState = {this.addFileToState}/>
+
+      </div>
+
+
+
+
+
+
       </div>
     )
   }
--- src/MyDropzone2.js
+++ src/MyDropzone2.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import {useDropzone} from 'react-dropzone';
 
 export default function MyDropzone2(props) {
+  const [files, setFiles] = useState([]);
+
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => {
+        return Object.assign(file, {preview: URL.createObjectURL(file)})
+      }
+    ));
+  },
     getFilesFromEvent: event => myCustomFileGetter(event, props)
   });
 
-  const files = acceptedFiles.map(f => (
-    <li key={f.name}>
-      {f.name} has <strong>myProps</strong>: {f.myProp === true ? 'YES' : ''}
-    </li>
+
+
+  const thumbs = files.map(file => (
+    <div key={file.name}>
+      <div>
+        <audio controls
+          src={file.preview}
+        />
+      </div>
+    </div>
   ));
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => file.preview);
+
+    // files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
 
   return (
     <section className="container">
@@ -18,10 +40,11 @@ export default function MyDropzone2(props) {
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
+
+        <aside>
+          {thumbs}
+        </aside>
+
     </section>
   );
 }
