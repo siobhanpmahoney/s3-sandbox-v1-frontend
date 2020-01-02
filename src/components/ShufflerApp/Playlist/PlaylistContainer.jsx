c946995cfe84094b1bf717264f63ed1788757b6f
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2020-01-02 16:16:13 -0500

    updating deps

--- src/components/ShufflerApp/Playlist/PlaylistContainer.jsx
+++ src/components/ShufflerApp/Playlist/PlaylistContainer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {withRouter} from 'react-router'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Loader from '../../utils/Loader'
 
 
 class PlaylistContainer extends React.Component {
@@ -8,14 +11,37 @@ class PlaylistContainer extends React.Component {
 
   }
 
+  componentDidMount() {
+    console.log("component mounting")
+  }
+
+  trackParser = () => {
+    return this.props.location.search.split("?tracks=")[1].split("&").map((couple) => {
+      return {song: couple.split("=")[0], version: couple.split("=")[1]}
+    })
+  }
+
   render() {
-    console.log(this.props)
-    return (
-      <div>
-        Playlist
-      </div>
+    console.log("in render — this.props: ", !!this.props.album)
+
+    return !this.props.album ? (
+        <Loader />
+    ) : (
+        <div>
+          Playlist
+        </div>
     )
+
+
+
+
   }
 }
 
-export default withRouter(PlaylistContainer)
+function mapStateToProps(state, props) {
+    return {
+      album: state.albums && state.albums.find((a) => a.id == props.albumId)
+    }
+  }
+
+export default withRouter(connect(mapStateToProps, {})(PlaylistContainer))
